34f0e8fee5f992e240840c17d5ac21d7c4a7209f
James <jkhallpurpose@gmail.com>
2025-01-11 23:20:45 -0500

    jan_11_a

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,15 @@
 import SectionHeaders from '../components/layout/header/SectionHeaders';
+import { StyledSeparator } from '../registry/new-york/ui/StyledSeparator';
 
 const About = () => {
   // check for inspiration: https://ca.shop.runningroom.com/en_ca/our-story
 
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="mt-16 mx-auto  py-4 w-full ">
+      <div className="mt-16 mx-auto  py-4 w-1/2 ">
         <section className="text-center my-20 " id="about">
           <SectionHeaders subHeader="Our Story" mainHeader="About Us" />
+          <StyledSeparator className="my-4" />
           <div>
             <div className="max-w-2xl mx-auto text-black mt-4 flex flex-col gap-4">
               <p>Serving the neighbourhood since 1990.</p>
