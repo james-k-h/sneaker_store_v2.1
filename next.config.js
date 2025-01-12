2e8094bafc78e9bae76f81dd1330348b82c92861
James <jkhallpurpose@gmail.com>
2025-01-11 20:20:58 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< dev
-=======
-  // experimental: {
-  //   missingSuspenseWithCSRBailout: false,
-  // },
->>>>>>> local
   images: {
     remotePatterns: [
       {
