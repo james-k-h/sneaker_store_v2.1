4f5ffc2a1374c12230f57402bb608a2e953ad066
James <jkhallpurpose@gmail.com>
2025-09-07 15:43:43 -0400

    sep 7 2025-7

--- src/app/components/Loading.jsx
+++ src/app/components/Loading.jsx
@@ -1,18 +1,15 @@
-import { PuffLoader } from 'react-spinners';
-
 const Loader = () => {
   return (
-    <div
-      className="
-      h-[70vh]
-      flex
-      flex-col
-      justify-center
-      items-center
-    "
-    >
-      <PuffLoader size={100} color="black" />
-      <p>Loading...</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <div className="relative">
+        {/* Spinning circle */}
+        <div className="w-16 h-16 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+      </div>
+
+      {/* Loading text */}
+      <p className="mt-4 text-lg font-medium text-gray-700">
+        Loading in progress...
+      </p>
     </div>
   );
 };
--- src/app/components/layout/home/index.js
+++ src/app/components/layout/home/index.js
@@ -1,9 +1,23 @@
+'use client';
 import LayoutOne from './LayoutOne';
 import LayoutTwo from './LayoutTwo';
 import { HomeFeaturedItems } from './HomeFeaturedItems';
 import SectionHeaders from '../header/SectionHeaders';
+import { useState, useEffect } from 'react';
+import Loading from '../../Loading';
 
 const FeaturedSneakers = () => {
+  const [homeFeaturedItems, setHomeFeaturedItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch('/api/home-featured').then((res) => {
+      res
+        .json()
+        .then((homeFeaturedItems) => setHomeFeaturedItems(homeFeaturedItems));
+    });
+    setIsLoading(true);
+  }, []);
   return (
     <section
       className="w-full mt-16 sm:mt-24  md:mt-32 px-5 
@@ -16,15 +30,42 @@ const FeaturedSneakers = () => {
       />
 
       <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
-        <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
-          <LayoutOne item={HomeFeaturedItems[0]} />
-        </article>
-        <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
+        {/* solo */}
+        {isLoading === false ? (
+          <Loading />
+        ) : (
+          homeFeaturedItems
+            .filter((item) => item.filter === 'solo')
+            .map((image) => (
+              <article
+                key={image._id}
+                className=" col-span-2  sxl:col-span-1 row-span-2 relative"
+              >
+                <LayoutOne item={image} />
+              </article>
+            ))
+        )}
+        {/* double */}
+        {isLoading === false ? (
+          <Loading />
+        ) : (
+          homeFeaturedItems
+            .filter((item) => item.filter === 'single')
+            .map((image) => (
+              <article
+                key={image._id}
+                className=" col-span-2 sm:col-span-1 row-span-1 relative"
+              >
+                <LayoutTwo item={image} />
+              </article>
+            ))
+        )}
+        {/* <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
           <LayoutTwo item={HomeFeaturedItems[1]} />
         </article>
         <article className="col-span-2 sm:col-span-1 row-span-1 relative">
           <LayoutTwo item={HomeFeaturedItems[2]} />
-        </article>
+        </article> */}
       </div>
     </section>
   );
